bdfc27bf5c885bd446eaccd06b5edd874f8f04db
iBlu_HD <jlmartin9000@gmail.com>
2023-09-26 10:44:28 +0100

    added frequencies accumulator

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,14 @@
 
 def frequencies(items):
     frequencies = {}
+    for temp in items:
+        word = str(temp)
+        if word in frequencies.keys():
+            frequencies[word] += 1
+        else:
+            frequencies[word] = 1
+    
     # Your code goes here
     return frequencies
+
+
